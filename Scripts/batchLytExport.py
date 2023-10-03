cb2f1c09d64578efcb4392e18d8ea5e5101ba581
Яромир Шаталов <ya.shatalov@geoplan.local>
2023-10-03 12:28:48 +0300

    add changeimgsize

---
+++ Scripts/batchLytExport.py
@@ -0,0 +1,46 @@
+import arcpy
+import os
+
+# def getLyts(value, aprx):
+#     lyt_name = value.value
+#     lyt = aprx.listLayouts(lyt_name)[0]
+#     return lyt
+
+def exportToJPEG(lyts, dpi, quality, outputFolder):
+    for lyt in lyts:
+        try:
+            lyt.exportToJPEG(os.path.join(outputFolder, lyt.name), dpi, jpeg_quality=quality)
+            arcpy.AddMessage(f'{lyt.name}: Успешно экспортирован')
+        except Exception as e:
+            arcpy.AddMessage(f'{lyt.name}: {e}')
+
+def exportToPDF(lyts, dpi, quality, output_as_image, outputFolder):
+    for lyt in lyts:
+        try:
+            lyt.exportToPDF(os.path.join(outputFolder, lyt.name), dpi, image_compression='LZW', layer_attributes='LAYERS_ONLY', georef_info=False, jpeg_compression_quality=quality, output_as_image=output_as_image)
+            arcpy.AddMessage(f'{lyt.name}: Успешно экспортирован')
+        except Exception as e:
+            arcpy.AddMessage(f'{lyt.name}: {e}')
+
+
+
+def execute():
+    lyt_names = arcpy.GetParameterAsText(0)
+    outputFormat = arcpy.GetParameterAsText(1)
+    outputAsImage = arcpy.GetParameter(2)
+    dpi = arcpy.GetParameter(3)
+    quality = arcpy.GetParameter(4)
+    outputFolder = arcpy.GetParameterAsText(5)
+
+    aprx = arcpy.mp.ArcGISProject("CURRENT")
+    lyts = [lyt for lyt in aprx.listLayouts() if lyt.name in lyt_names]
+
+    if outputFormat == 'JPEG':
+        exportToJPEG(lyts, dpi, quality, outputFolder, aprx)
+    elif outputFormat == 'PDF':
+        exportToPDF(lyts, dpi, quality, outputAsImage, outputFolder, aprx)
+    else:
+        arcpy.AddMessage(f'Формат {outputFormat} не поддерживается')
+
+if __name__ == '__main__':
+    execute()
--- Scripts/changeImgSize.py
+++ Scripts/changeImgSize.py
@@ -7,6 +7,13 @@ def isImage(f: str) -> bool:
     if f.endswith('.jpg'):
         return True
 
+def changeSize(img, new_dpi):
+    dpi = img.info['dpi'][0]
+    dpiDelta = new_dpi / dpi
+    new_width = int(round(img.width * dpiDelta))
+    new_height = int(round(img.height * dpiDelta))
+    return img.resize((new_width, new_height), Image.LANCZOS)
+
 def execute():
     dirname = arcpy.GetParameterAsText(0)
     dpi = arcpy.GetParameter(1)
@@ -17,6 +24,7 @@ def execute():
             try:
                 file = os.path.join(r, f)
                 img = Image.open(file)
+                img = changeSize(img, dpi)
                 img.save(file, dpi=(dpi,dpi))
                 raise NameError('НЕ ПАШЕТ')
             except Exception as e:

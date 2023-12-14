a6ecb0cc6b27e092bd981556739555dc09d49b7c
ttruefix14 <ttruefix14@gmail.com>
2023-12-14 09:52:08 +0300

    Добавил инструмент deleteEmptyShapefiles

---
+++ Scripts/deleteEmptyShapefiles.py
@@ -0,0 +1,19 @@
+import arcpy
+import os
+
+arcpy.env.workspace = arcpy.GetParameterAsText(0)
+arcpy.env.addOutputsToMap = False
+
+for r, dirname, filenames in os.walk(arcpy.env.workspace):
+    arcpy.env.workspace = r
+
+    shapefiles = arcpy.ListFeatureClasses()
+
+    for shapefile in shapefiles:
+        layerName = arcpy.Describe(shapefile).baseName
+        arcpy.AddMessage(layerName)
+        arcpy.MakeFeatureLayer_management(shapefile, layerName)
+        notHasFeatures = arcpy.GetCount_management(layerName).getOutput(0)
+        arcpy.AddMessage(str(notHasFeatures))
+        if int(arcpy.GetCount_management(layerName).getOutput(0)) == 0:
+            arcpy.Delete_management(shapefile)
